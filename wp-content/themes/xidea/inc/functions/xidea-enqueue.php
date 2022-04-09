f07602cf66083379e4650f390bb08c76cf8da81a
Arkenon <kadimgultekin@gmail.com>
2022-04-09 23:46:56 +0300

    Basic features created...

--- wp-content/themes/xidea/inc/functions/xidea-enqueue.php
+++ wp-content/themes/xidea/inc/functions/xidea-enqueue.php
@@ -6,31 +6,21 @@
 if ( ! function_exists( 'xidea_enqueue_styles' ) ) :
 
 	function xidea_enqueue_styles() {
-		wp_register_style( 'themify-icons', XIDEA_URI . '/assets/css/themify-icons.css', [], XIDEA_VERSION );
-		wp_register_style( 'font-awesome', XIDEA_URI . '/assets/css/font-awesome.min.css', [], XIDEA_VERSION );
-		wp_register_style( 'flaticon', XIDEA_URI . '/assets/css/flaticon.css', [], XIDEA_VERSION );
+
 		wp_register_style( 'style', XIDEA_URI . '/assets/css/style.css', [], XIDEA_VERSION );
-		wp_register_style( 'xidea-blocks', XIDEA_URI . '/assets/css/blocks.css', ['style'], XIDEA_VERSION );
 		wp_register_style( 'bootstrap', XIDEA_URI . '/assets/css/bootstrap.min.css', [], XIDEA_VERSION );
+		wp_register_style( 'bootstrap-icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css', [], XIDEA_VERSION );
 
-		wp_enqueue_style( 'themify-icons' );
-		wp_enqueue_style( 'font-awesome.' );
-		wp_enqueue_style( 'flaticon' );
 		wp_enqueue_style( 'style' );
-		wp_enqueue_style( 'xidea-blocks' );
 		wp_enqueue_style( 'bootstrap' );
+		wp_enqueue_style( 'bootstrap-icons' );
 
-		wp_register_script('bootstrap-bundle',XIDEA_URI . '/assets/js/bootstrap.bundle.min.js',['jquery'],XIDEA_VERSION,true);
-		wp_register_script('jquery-ui',XIDEA_URI . '/assets/js/jquery-ui.min.js',['jquery'],XIDEA_VERSION,true);
-		wp_register_script('jquery-plugins',XIDEA_URI . '/assets/js/jquery-plugin-collection.js',['jquery'],XIDEA_VERSION,true);
-		wp_register_script('custom',XIDEA_URI . '/assets/js/script.js',['jquery'],XIDEA_VERSION,true);
-		wp_register_script('modernizr',XIDEA_URI . '/assets/js/modernizr.custom.js',['jquery'],XIDEA_VERSION,true);
+		wp_register_script( 'bootstrap-bundle', XIDEA_URI . '/assets/js/bootstrap.bundle.min.js', [ 'jquery' ], XIDEA_VERSION, true );
+		wp_register_script( 'custom', XIDEA_URI . '/assets/js/custom.js', [ 'jquery' ], XIDEA_VERSION, true );
 
-		wp_enqueue_script('bootstrap-bundle');
-		wp_enqueue_script('jquery-ui');
-		wp_enqueue_script('jquery-plugins');
-		wp_enqueue_script('custom');
-		wp_enqueue_script('modernizr');
+		wp_enqueue_script( 'bootstrap-bundle' );
+		wp_enqueue_script( 'custom' );
+
 	}
 
 	add_action( 'wp_enqueue_scripts', 'xidea_enqueue_styles' );
@@ -43,13 +33,18 @@ endif;
 if ( ! function_exists( 'xidea_editor_styles' ) ) :
 
 	function xidea_editor_styles() {
-		add_editor_style( array( './assets/css/blocks.css' ) );
-		add_editor_style( array( './assets/css/style.css' ) );
-		add_editor_style( array( './assets/css/bootstrap.min.css' ) );
+		$request_uri     = stristr( $_SERVER['REQUEST_URI'], 'site-editor.php' );
+		if ( false !== $request_uri ) {
+			add_editor_style( array( './assets/css/style.css' ) );
+			add_editor_style( array( './assets/css/bootstrap.min.css' ) );;
+		}
 	}
 
-	add_action( 'admin_init', 'xidea_editor_styles' );
+	add_action( 'init', 'xidea_editor_styles' );
+	add_action( 'pre_get_posts', 'xidea_editor_styles' );
 
 endif;
+
+
 
 
--- wp-content/themes/xidea/inc/functions/xidea-setup.php
+++ wp-content/themes/xidea/inc/functions/xidea-setup.php
@@ -8,7 +8,28 @@ if ( ! function_exists( 'xidea_setup' ) ) :
 		add_theme_support( 'wp-block-styles' );
 		add_theme_support( 'post-thumbnails' );
 		add_theme_support( 'editor-styles' );
-		add_theme_support( 'html5', array( 'comment-form', 'comment-list' ) );
+		add_theme_support( 'automatic-feed-links' );
+		add_theme_support( 'title-tag' );
+
+		register_nav_menus(
+			array(
+				'primary' => esc_html__( 'Primary', 'xidea' ),
+			)
+		);
+
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+				'style',
+				'script',
+			)
+		);
+
 		//Translations
 		load_theme_textdomain( 'xidea', get_template_directory() . '/languages' );
 	}
