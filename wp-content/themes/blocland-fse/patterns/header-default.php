e5f41e9b641c3e9a873792c024fc555aa7c681d9
kadim.gultekin <kadimgultekin@gmail.com>
2022-12-01 16:51:43 +0300

    Blocland FSE

--- wp-content/themes/blocland-fse/patterns/header-default.php
+++ wp-content/themes/blocland-fse/patterns/header-default.php
@@ -11,8 +11,7 @@
 <header id="header" class="wp-block-group" style="padding-top:20px;padding-right:10px;padding-bottom:20px;padding-left:10px"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
     <div class="wp-block-group alignwide"><!-- wp:site-title {"textAlign":"left","style":{"typography":{"textDecoration":"none"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"fontSize":"large-2"} /-->
 
-        <!-- wp:navigation {"ref":1829,"layout":{"type":"flex","justifyContent":"center"},"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"spacing":{"blockGap":"var:preset|spacing|40"}},"fontSize":"medium"} /--></div>
+        <!-- wp:navigation {"layout":{"type":"flex","justifyContent":"center"},"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"spacing":{"blockGap":"var:preset|spacing|40"}},"fontSize":"medium"} /--></div>
     <!-- /wp:group --></header>
 <!-- /wp:group -->
-
 
