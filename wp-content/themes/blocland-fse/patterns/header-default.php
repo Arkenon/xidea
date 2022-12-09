616d3e6143b75cc5f42938082167390b3b26ea68
kadim.gultekin <kadimgultekin@gmail.com>
2022-12-09 13:37:15 +0300

    Blocland FSE = 1.0.6 = * Header menu ref cleaned

--- wp-content/themes/blocland-fse/patterns/header-default.php
+++ wp-content/themes/blocland-fse/patterns/header-default.php
@@ -11,7 +11,7 @@
 <header id="header" class="wp-block-group" style="border-top-color:var(--wp--preset--color--foreground);border-top-width:3px;padding-top:20px;padding-right:10px;padding-bottom:20px;padding-left:10px"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
     <div class="wp-block-group alignwide"><!-- wp:site-title {"textAlign":"left","style":{"typography":{"textDecoration":"none"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"fontSize":"large-2"} /-->
 
-        <!-- wp:navigation {"ref":2394,"layout":{"type":"flex","justifyContent":"center"},"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"spacing":{"blockGap":"var:preset|spacing|40"}},"fontSize":"medium"} /-->
+        <!-- wp:navigation {"layout":{"type":"flex","justifyContent":"center"},"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"spacing":{"blockGap":"var:preset|spacing|40"}},"fontSize":"medium"} /-->
 
         <!-- wp:social-links {"iconColor":"secondary","iconColorValue":"#362706","iconBackgroundColor":"light","iconBackgroundColorValue":"#E9E5D6","size":"has-normal-icon-size","className":"blocland-hide-small-devices","layout":{"type":"flex","flexWrap":"nowrap"}} -->
         <ul class="wp-block-social-links has-normal-icon-size has-icon-color has-icon-background-color blocland-hide-small-devices"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
