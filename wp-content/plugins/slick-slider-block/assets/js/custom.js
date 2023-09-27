8f3af240cb064e4a44c45b2e4208c6d4a031b31d
kadim.gultekin <kadimgultekin@gmail.com>
2023-09-27 14:26:06 +0300

    some files deleted

--- wp-content/plugins/slick-slider-block/assets/js/custom.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-
-	$(".gb-for-slick-slider").not('.slick-initialized').slick();
-	$(".wp-block-gb-for-slick-slider-slick-slider").not('.slick-initialized').slick();
-
-})(window.jQuery);
-
